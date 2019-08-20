dfac3135882149d32074def6c2e987f24d59c13e
Corual <Corual@CORUAL-PC>
2019-08-20 21:02:49 +0800

    cmd调用例子

--- ManjuuPing/Program.cs
+++ ManjuuPing/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace ManjuuPing
 {
@@ -7,6 +10,36 @@ namespace ManjuuPing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            PingCoreCode();
+            Console.ReadLine();
+        }
+
+        public static async void PingCoreCode()
+        {
+            try
+            {
+                using (Process process = new Process())
+                {
+                    ProcessStartInfo startInfo = process.StartInfo;
+                    startInfo.FileName = "ping";
+                    startInfo.Arguments = "www.baidu.com";
+                    startInfo.RedirectStandardOutput = true;
+                    //startInfo.StandardOutputEncoding = Encoding.UTF8;
+                    process.Start();
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = await reader.ReadToEndAsync();
+                        Console.WriteLine(result);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
