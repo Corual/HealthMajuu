429cc709d990895a498c95828701f1a6eb2a3089
Corual Win7_Desk <corual_cl@163.com>
2019-08-30 01:23:57 +0800

    ?

--- ManjuuPing/PingHostedService.cs
+++ ManjuuPing/PingHostedService.cs
@@ -1,5 +1,7 @@
 ﻿using ManjuuApplications;
 using ManjuuCommon.ILog;
+using ManjuuCommon.ILog.NLog;
+using ManjuuDomain.Dto;
 using Microsoft.Extensions.Hosting;
 using NLog;
 using System;
@@ -25,12 +27,24 @@ namespace ManjuuPing
         }
 
         #region 启动
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
             //throw new NotImplementedException();
+            NLogMgr.DebugLog(_programLog, "正在加载工具配置");
+
+            var toolSetting = await LoadToolConfigAsync();
+
+            if (null == toolSetting)
+            {
+                NLogMgr.DebugLog(_programLog, "尚未有可用配置，进入等待状态");
+                //todo:使用定时框架，定时获取配置
+                return;
+            }
 
 
-            return Task.CompletedTask;
+            //todo:根据配置，生成定时任务
+
+            return ;
         }
         #endregion
 
@@ -44,8 +58,11 @@ namespace ManjuuPing
         #endregion
 
 
-        #region MyRegion
-
+        #region 加载工具配置
+        public async Task<ToolConfigDto> LoadToolConfigAsync()
+        {
+            return await _toolConfigApplication.GetToolValidConfigAsync();
+        }
         #endregion
 
     }
