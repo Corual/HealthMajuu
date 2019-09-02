b866f265c40ac8ca01f32a25c54d2bce317cee64
Corual <corual_cl@163.com>
2019-09-02 12:27:06 +0800

    Sqlite数据迁移

---
+++ ManjuuInfrastructure/Migrations/20190902040712_Init.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManjuuInfrastructure.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobConfigurations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(nullable: false),
+                    State = table.Column<int>(nullable: false),
+                    StartToWrokTime = table.Column<DateTime>(nullable: true),
+                    StopToWorkTime = table.Column<DateTime>(nullable: true),
+                    WorkSpan = table.Column<int>(nullable: true),
+                    PresetTimeout = table.Column<int>(nullable: false),
+                    PingSendCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobConfigurations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MachineInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(nullable: false),
+                    State = table.Column<int>(nullable: false),
+                    IpAddressV4 = table.Column<string>(nullable: true),
+                    Port = table.Column<string>(nullable: true),
+                    Remarks = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MachineInfos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobConfigurations");
+
+            migrationBuilder.DropTable(
+                name: "MachineInfos");
+        }
+    }
+}
